1ad3f4bda6bf4e994e0f6110c1953b1371d5eefa
Rodrigo Albuquerque <106634333+Rohdev23@users.noreply.github.com>
2023-09-18 23:47:57 -0300

    Atualizar o kotlinb.sh

--- kotlinb.sh
+++ kotlinb.sh
@@ -1,21 +1,29 @@
- #!//data/data/com.termux/files/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash
 
- # Verifica se o número de argumentos é menor que 1
- if [ $# -lt 1 ]; then
-     echo "Uso: $0 <arquivo.kt>"
-     exit 1
- fi
+# Verifica se o número de argumentos é menor que 1
+if [ $# -lt 1 ]; then
+    echo "Uso: $0 <arquivo.kt>"
+    exit 1
+fi
 
- # Obtém o nome do arquivo sem a extensão
- filename=$(basename -- "$1")
- filename="${filename%.*}"
+# Obtém o nome do arquivo sem a extensão
+filename=$(basename -- "$1")
+filename="${filename%.*}"
 
- # Compila o código Kotlin
- kotlinc $1 -include-runtime -d $filename.jar
+# Compila o código Kotlin
+kotlinc $1 -include-runtime -d "$filename.jar"
 
- # Verifica se a compilação foi bem-sucedida
- if [ $? -eq 0 ]; then
-     echo "Compilação concluída com sucesso. Arquivo $filename.jar criado."
- else
-     echo "Erro durante a compilação."
- fi
+# Verifica se a compilação foi bem-sucedida
+if [ $? -eq 0 ]; then
+    echo "Compilação concluída com sucesso. Arquivo $filename.jar criado."
+
+    # Pergunta ao usuário se deseja executar o arquivo .jar
+    read -p "Deseja executar o arquivo $filename.jar? (s/n) " choice
+    case "$choice" in 
+      s|S ) java -jar "$filename.jar";;
+      n|N ) echo "Execução do arquivo $filename.jar cancelada.";;
+      * ) echo "Escolha inválida. Saindo.";;
+    esac
+else
+    echo "Erro durante a compilação."
+fi
